11c45c612e848f909d8acbdb2f4c8e81fa782879
Sean Cao <seanc.w3@gmail.com>
2026-02-05 19:32:49 -0500

    initial commit

--- maizeBusConvert.py
+++ maizeBusConvert.py
@@ -1,2 +1,18 @@
+# import fitz
+# source venv/bin/activate to run in venv
+# switch environment to venv at bottom right
+# deactivate to exit venv
+# pip install pymupdf pyinstaller
+# 
+import fitz
+
+
 print("hello world")
 
+
+doc = fitz.open("Map.pdf")
+
+for page in doc:
+    text = page.get_text()
+    print(text)
+doc.close()
